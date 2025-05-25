eda5dfc18115550ba981cf7aceb4ac813c74db9d
Limon00001 <limonhossain82@gmail.com>
2025-05-25 20:15:25 +0600

    feat(client): implement not found page

---
+++ client/src/pages/not-found/index.jsx
@@ -0,0 +1,13 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 25 May, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+const NotFoundPage = () => {
+  return <div>NotFoundPage</div>;
+};
+
+// Export
+export default NotFoundPage;
