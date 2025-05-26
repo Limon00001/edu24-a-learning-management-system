2150ae4eec491d318cef5b3cec5e8c28fb951cc2
Limon00001 <limonhossain82@gmail.com>
2025-05-26 19:19:49 +0600

    feat(client): add instructor context and provider for managing course data

---
+++ client/src/context/instructor-context/index.jsx
@@ -0,0 +1,44 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 26 May, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// External Imports
+import { createContext, useState } from 'react';
+
+// Internal Imports
+import {
+  courseCurriculumInitialFormData,
+  courseLandingInitialFormData,
+} from '@/config';
+
+// Create Instructor Context
+const InstructorContext = createContext(null);
+
+// Instructor Provider
+const InstructorProvider = ({ children }) => {
+  const [courseLandingFormData, setCourseLandingFormData] = useState(
+    courseLandingInitialFormData,
+  );
+  const [courseCurriculumFormData, setCourseCurriculumFormData] = useState(
+    courseCurriculumInitialFormData,
+  );
+
+  const value = {
+    courseLandingFormData,
+    setCourseLandingFormData,
+    courseCurriculumFormData,
+    setCourseCurriculumFormData,
+  };
+
+  return (
+    <InstructorContext.Provider value={value}>
+      {children}
+    </InstructorContext.Provider>
+  );
+};
+
+// Export
+export { InstructorContext, InstructorProvider };
