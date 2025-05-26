6b842e23f6a729879c3f0985310f501349f74854
Limon00001 <limonhossain82@gmail.com>
2025-05-26 19:32:38 +0600

    feat(server): implement cloudinary helper functions for media upload and deletion

---
+++ server/helpers/cloudinary.js
@@ -0,0 +1,38 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 26 May, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// External Import
+import { v2 as cloudinary } from 'cloudinary';
+
+// Cloudinary Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadMediaToCloudinary = async (filePath) => {
+  try {
+    const result = await cloudinary.uploader.upload(filePath, {
+      resource_type: 'auto',
+    });
+    return result;
+  } catch (error) {
+    throw new Error(`Error uploading file to clodinary ${error}`);
+  }
+};
+
+const deleteMediaFromCloudinary = async (publicId) => {
+  try {
+    await cloudinary.uploader.destroy(publicId);
+  } catch (error) {
+    throw new Error(`Failed deleting file from clodinary ${error}`);
+  }
+};
+
+// Export
+export { deleteMediaFromCloudinary, uploadMediaToCloudinary };
